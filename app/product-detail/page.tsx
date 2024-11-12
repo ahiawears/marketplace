d896a3c24ae3dba0f885a1803d53ac7701ce4f6c
Ikenna Kingsley Eze <kingsley2169@gmail.com>
2024-11-12 16:02:24 +0200

    added the product detail page image/thumnail component

---
+++ app/product-detail/page.tsx
@@ -0,0 +1,12 @@
+import ProductItem from '@/components/ui/product-item-detail'
+import React from 'react'
+
+const ProductDetail: React.FC = () => {
+    return (
+        <div>
+            <ProductItem />
+        </div>
+    )
+}
+
+export default ProductDetail
---
+++ components/ui/product-item-detail.tsx
@@ -0,0 +1,51 @@
+"use client";
+
+import React, { useState} from 'react'
+
+const ProductItem = () => {
+    const productImages = [
+        'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-01.jpg',
+        'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-02.jpg',
+        'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-03.jpg',
+        'https://tailwindui.com/plus/img/ecommerce-images/product-page-01-related-product-04.jpg',
+    ];
+
+    // Set the main image to the first thumbnail initially
+    const [mainImage, setMainImage] = useState(productImages[0]);
+    return (
+        <div>
+            <div className="container mx-auto py-10 flex flex-col lg:flex-row">
+                <div className="lg:basis-3/5 p-4">
+                    <div className="flex justify-center mb-4">
+                        <img
+                            src={mainImage}
+                            alt="Main Product"
+                            className="w-full max-w-xl object-cover rounded-lg"
+                        />
+                    </div>
+                    {/* Thumbnails */}
+                    <div className="flex justify-center gap-4">
+                        {productImages.map((image, index) => (
+                            <div
+                                key={index}
+                                className="cursor-pointer border-2 border-transparent hover:border-gray-400 rounded-md overflow-hidden"
+                                onClick={() => setMainImage(image)}
+                            >
+                                <img
+                                    src={image}
+                                    alt={`Thumbnail ${index + 1}`}
+                                    className="w-20 h-20 object-cover"
+                                />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                <div className="lg:basis-2/5 p-4">
+                    
+                </div>
+            </div> 
+        </div>
+    )
+}
+
+export default ProductItem
