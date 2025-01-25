4b6489a76b61265424803d783b3ebed5f843a1df
Ikenna Kingsley Eze <kingsley2169@gmail.com>
2025-01-25 06:09:59 +0200

    aligned bento grid

--- components/landingcomps/category-bento-section.tsx
+++ components/landingcomps/category-bento-section.tsx
@@ -12,17 +12,17 @@ export const CategoryBento = () => {
                     Everything you need to deploy your app
                 </p>
                 <div className="mt-10 grid gap-4 sm:mt-16 lg:grid-cols-3 lg:grid-rows-1">
-                    <div className="mx-auto lg:basis-1/3 md:basis-2/3 basis-10/12">
+                    <div className="lg:basis-1/3 md:basis-2/3 basis-10/12 sm:justify-items-center lg:justify-self-start">
                         <div className="relative mb-8 block cursor-pointer">
                             <Image 
                                 width={370}
                                 height={470}
                                 alt="dress"
                                 src={"https://placehold.co/370x470.png?text=Drop+the+products+main+image+here+or+click+here+to+browse"}
-                                className="inset-0 transition duration-500 ease-in-out hover:h-[475px] hover:w-[375px]"
+                                className="justify-self-center inset-0 transition duration-500 ease-in-out hover:h-[475px] hover:w-[375px]"
                                 priority
                             />
-                            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44 m-0">
+                            <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44">
                                 <Button className="flex justify-center items-center w-full">
                                     Dresses
                                 </Button>
@@ -35,7 +35,7 @@ export const CategoryBento = () => {
                                 height={350}
                                 alt="sunglasses"
                                 src={"https://placehold.co/370x350.png?text=Drop+the+products+main+image+here+or+click+here+to+browse"}
-                                className="  inset-0 transition duration-500 ease-in-out hover:h-[355px] hover:w-[375px]"
+                                className="justify-self-center inset-0 transition duration-500 ease-in-out hover:h-[355px] hover:w-[375px]"
                                 priority
                             />
                             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44">
@@ -47,18 +47,14 @@ export const CategoryBento = () => {
 
                     </div>
 
-
-
-
-
-                    <div className="mx-auto lg:basis-1/3 md:basis-2/3 basis-10/12">
+                    <div className="lg:basis-1/3 md:basis-2/3 basis-10/12 sm:justify-items-center lg:justify-self-center">
                         <div className="relative mb-8 block cursor-pointer">
                             <Image 
                                 width={370}
                                 height={350}
                                 alt="watches"
                                 src={"https://placehold.co/370x350.png?text=Drop+the+products+main+image+here+or+click+here+to+browse"}
-                                className=" inset-0 transition duration-500 ease-in-out hover:h-[355px] hover:w-[375px]"
+                                className="justify-self-center inset-0 transition duration-500 ease-in-out hover:h-[355px] hover:w-[375px]"
                                 priority
                             />
                             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44">
@@ -74,7 +70,7 @@ export const CategoryBento = () => {
                                 height={470}
                                 alt="watches"
                                 src={"https://placehold.co/370x470.png?text=Drop+the+products+main+image+here+or+click+here+to+browse"}
-                                className=" inset-0 transition duration-500 ease-in-out hover:h-[475px] hover:w-[375px]"
+                                className="justify-self-center inset-0 transition duration-500 ease-in-out hover:h-[475px] hover:w-[375px]"
                                 priority
                             />
                             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44">
@@ -85,18 +81,14 @@ export const CategoryBento = () => {
                         </div>
                     </div>
 
-
-
-
-
-                    <div className="mx-auto lg:basis-1/3 md:basis-2/3 basis-10/12">
+                    <div className="lg:basis-1/3 md:basis-2/3 basis-10/12 sm:justify-items-center lg:justify-self-end">
                         <div className="relative mb-8 block cursor-pointer">
                             <Image 
                                 width={370}
                                 height={470}
                                 alt="bags"
                                 src={"https://placehold.co/370x470.png?text=Drop+the+products+main+image+here+or+click+here+to+browse"}
-                                className=" inset-0 transition duration-500 ease-in-out hover:h-[475px] hover:w-[375px]"
+                                className="justify-self-center inset-0 transition duration-500 ease-in-out hover:h-[475px] hover:w-[375px]"
                                 priority
                             />
                             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44">
@@ -112,7 +104,7 @@ export const CategoryBento = () => {
                                 height={350}
                                 alt="watches"
                                 src={"https://placehold.co/370x350.png?text=Drop+the+products+main+image+here+or+click+here+to+browse"}
-                                className=" inset-0 transition duration-500 ease-in-out hover:h-[355px] hover:w-[375px]"
+                                className="justify-self-center inset-0 transition duration-500 ease-in-out hover:h-[355px] hover:w-[375px]"
                                 priority
                             />
                             <div className="absolute left-1/2 transform -translate-x-1/2 bottom-5 w-44">
